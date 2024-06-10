ca146690850a4a2b821ea0b775005c299058e726
MattiaMessana <157867689+MattiaMessana@users.noreply.github.com>
2024-06-10 15:43:26 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import './bootstrap';
+import "~resources/scss/app.scss";
+import * as bootstrap from "bootstrap";
+import '@fortawesome/fontawesome-free/js/all.min.js';
+import.meta.glob(["../img/**"]);
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    {{-- includiamo assets con le direttive @vite  --}}
+    @vite('resources/js/app.js')
+
+</head>
+<body>
+    <h1>ciao sono template di laravel <i class="fa-brands fa-laravel"></i> con boostrap <i class="fa-brands fa-bootstrap"></i> e fontawesome <i class="fa-solid fa-font-awesome"></i></h1>
+</body>
+</html>
